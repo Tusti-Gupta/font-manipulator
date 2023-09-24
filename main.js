0475651aa6ea2888413fe2bc72b200bbcd8587d0
Tusti-Gupta <86391913+Tusti-Gupta@users.noreply.github.com>
2023-09-24 18:14:23 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
+leftWristX=0;
+rightWristX=0;
+difference = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 550);
 
     canvas = createCanvas(550, 410);
-    canvas.position(980, 185);
+    canvas.position(965, 185);
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
@@ -11,6 +15,9 @@ function setup() {
 
 function draw() {
     background('#969A97');
+    textSize(difference);
+    fill('orange');
+    text("Play with me!", 275, 205);
 }
 
 function modelLoaded() {
@@ -20,5 +27,8 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = Math.floor(leftWristX - rightWristX);
     }
 }
